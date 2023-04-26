c4ee2b1a5b5e4c73ce69984e436ffd9c92ec7049
Betico1928 <betoveluis@gmail.com>
2023-04-26 07:44:08 -0500

    Proyecto creado

---
+++ app/src/main/java/javeriana/edu/co/taller3_compumovil/MainActivity.kt
@@ -0,0 +1,13 @@
+package javeriana.edu.co.taller3_compumovil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
