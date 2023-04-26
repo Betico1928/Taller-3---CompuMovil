12381fbc12ff8c0c717b2888f71858833f10a20c
Betico1928 <betoveluis@gmail.com>
2023-04-26 08:27:45 -0500

    Hacer GoogleMaps más bonito (cambio de estilo)

--- app/src/main/java/javeriana/edu/co/taller3_compumovil/MapsActivity.kt
+++ app/src/main/java/javeriana/edu/co/taller3_compumovil/MapsActivity.kt
@@ -1,20 +1,22 @@
 package javeriana.edu.co.taller3_compumovil
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
 import javeriana.edu.co.taller3_compumovil.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
-private lateinit var binding: ActivityMapsBinding
+    private lateinit var binding: ActivityMapsBinding
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -31,15 +33,33 @@ private lateinit var binding: ActivityMapsBinding
     {
         mMap = googleMap
 
+        cambiarEstilo()
         leerArchivoJSON()
+
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
+    private fun cambiarEstilo()
+    {
+        val googleMap = mMap
+        try
+        {
+            val success = googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.map_style_retro))
+        }
+        catch (e: Resources.NotFoundException)
+        {
+            Log.e("Taller 3", "Can't find style. Error: ", e)
+        }
+    }
+
     private fun leerArchivoJSON()
     {
-        TODO("Not yet implemented")
+        val googleMap = mMap
+        val fileName = "locations.json"
+
+        // Hola Bob
     }
 }
